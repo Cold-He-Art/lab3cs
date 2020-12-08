3bc8808689e7e91c71ef7fe43067dc9e15136069
Karina <kartinezmiss@gmail.com>
2020-12-08 22:30:50 +0300

    cs 3lab

---
+++ CSService/ConfigManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CSService
+{
+    class ConfigManager
+    {
+        private JsonParser jsonObject;
+        private XmlParser xmlObject;
+        private List<Functional> func;
+
+        public ConfigManager()
+        {
+            jsonObject = new JsonParser();
+            xmlObject = new XmlParser();
+            func = new List<Functional>();
+        }
+
+        internal List<Functional> GetOptions()
+        {
+            string[] files = new string[50];
+            files = Directory.GetFiles(@"C:\Users\karti\source\repos\NashService");
+            foreach (string file in files)
+            {
+                if (Path.GetExtension(file) == ".json") func = jsonObject.Parse();
+                else if (Path.GetExtension(file) == ".xml") func = xmlObject.Parse();
+            }
+
+            return func;
+        }
+
+    }
+}
---
+++ CSService/Parser.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Text.Json;
+
+namespace CSService
+{
+    public interface IParser 
+    {
+        List<Functional> Parse();
+    }
+
+
+    internal class XmlParser : IParser
+    {
+        public virtual List<Functional> Parse()
+        {
+            string Xmlpath = @"C:\Users\karti\source\repos\NashService\config.xml";
+            List<Functional> fullXMLOptions = new List<Functional>(); 
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Functional));
+            Functional XmlOptions = new Functional();
+
+            using (var xmlRead = new FileStream(Xmlpath, FileMode.OpenOrCreate))
+            {
+                XmlOptions = (Functional)xmlSerializer.Deserialize(xmlRead);
+            }
+
+            if (XmlOptions != null) 
+                fullXMLOptions.Add(XmlOptions);
+            else
+                throw new NullReferenceException();
+
+
+            return fullXMLOptions;
+        }
+
+    }
+
+
+    internal class JsonParser : IParser
+    {
+        private List<Functional> optionsJson = new List<Functional>(); 
+        private string jsonString; 
+        public virtual List<Functional> Parse()
+        {
+            using (var jsonStream = new StreamReader(@"C:\Users\karti\source\repos\NashService\appsettings.json"))
+            {
+                jsonString = jsonStream.ReadToEnd();
+            }
+            Functional optionJson = JsonSerializer.Deserialize<Functional>(jsonString);
+
+            if (optionJson != null) optionsJson.Add(optionJson);
+            else throw new NullReferenceException();
+
+            return optionsJson;
+        }
+    }
+}
